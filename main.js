c32f055a23a6b463a487c4c1388e329f572fe5a7
milkfish <i@mxd.moe>
2021-11-04 17:47:29 +0800

    Configuration is now supported

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow, globalShortcut} = require('electron')
 const path = require('path')
+const Store = require('electron-store');
+const prompt = require('electron-prompt');
 
 const SynologyAudioStationControlScript = {
   play: 'SYNO.SDS.AudioStation.Window.getPanelScope("SYNO.SDS.AudioStation.Main").audioPlayer.doPlay()',
@@ -12,8 +14,83 @@ const SynologyAudioStationControlScript = {
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let storeInstance = new Store();
 
-function createWindow() {
+function checkConfig() {
+  const url = storeInstance.get('url');
+  
+  if (!url) {
+    initConfig();
+  } else {
+    createWindow(url)
+  }
+}
+
+function initConfig() {
+  prompt({
+    title: 'Initialize',
+    label: 'Please enter your Audio Station URL, for example: https://nas/music',
+    value: 'https://nas/music',
+    inputAttrs: {
+      type: 'url'
+    },
+    type: 'input',
+    width: 800
+  })
+    .then((result) => {
+      if (result === null) {
+        app.exit();
+      } else {
+        storeInstance.set('url', result)
+        createWindow(storeInstance.get('url'))
+      }
+    })
+    .catch(console.error);
+}
+
+function editConfig() {
+  prompt({
+    title: 'Edit Configuration',
+    label: 'Please enter your Audio Station URL, for example: https://nas/music',
+    value: storeInstance.get('url') || '',
+    inputAttrs: {
+      type: 'url'
+    },
+    type: 'input',
+    width: 800
+  })
+    .then((result) => {
+      if (result !== null) {
+        storeInstance.set('url', result)
+        mainWindow.loadURL(result);
+      }
+    })
+    .catch(console.error);
+}
+
+function registerShortcut() {
+  globalShortcut.register('MediaPlayPause', () => {
+    mainWindow.webContents.executeJavaScript(SynologyAudioStationControlScript.play);
+  });
+  
+  globalShortcut.register('MediaPreviousTrack', () => {
+    mainWindow.webContents.executeJavaScript(SynologyAudioStationControlScript.previous);
+  });
+  
+  globalShortcut.register('MediaNextTrack', () => {
+    mainWindow.webContents.executeJavaScript(SynologyAudioStationControlScript.next);
+  });
+  
+  globalShortcut.register('MediaStop', () => {
+    mainWindow.webContents.executeJavaScript(SynologyAudioStationControlScript.stop);
+  });
+  
+  globalShortcut.register('CommandOrControl+E', () => {
+    editConfig()
+  })
+}
+
+function createWindow(url) {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
@@ -24,9 +101,10 @@ function createWindow() {
     title: 'Electron AudioStation'
   })
   
+  mainWindow.loadURL(url)  // you audio stations location
   // and load the index.html of the app.
-  mainWindow.loadURL('')  // you audio stations location
   
+  registerShortcut();
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
   
@@ -42,39 +120,20 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', checkConfig)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
+// app.on('window-all-closed', function () {
+//   // On macOS it is common for applications and their menu bar
+//   // to stay active until the user quits explicitly with Cmd + Q
+//   app.quit()
+// })
 
 app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow()
 })
-
-app.on('ready', () => {
-    const pause = globalShortcut.register('MediaPlayPause', () => {
-      mainWindow.webContents.executeJavaScript(SynologyAudioStationControlScript.play);
-    });
-    
-    const previous = globalShortcut.register('MediaPreviousTrack', () => {
-      mainWindow.webContents.executeJavaScript(SynologyAudioStationControlScript.previous);
-    });
-    
-    const next = globalShortcut.register('MediaNextTrack', () => {
-      mainWindow.webContents.executeJavaScript(SynologyAudioStationControlScript.next);
-    });
-    
-    const stop = globalShortcut.register('MediaStop', () => {
-      mainWindow.webContents.executeJavaScript(SynologyAudioStationControlScript.stop);
-    });
-  }
-)
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
